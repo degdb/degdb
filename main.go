cb468d7bf4ef560424d88693ced5c92d8de901b7
Tristan Rice <rice@fn.lc>
2015-09-05 06:27:08 +0000

    Updates

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/hashicorp/memberlist"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -17,6 +20,7 @@ import (
 
 var peerAddr = flag.String("peer", "", "The peer address to bootstrap off.")
 var bindPort = flag.Int("port", 7946, "The port to bind on.")
+var bindAddr = flag.String("hostname", "", "The hostname to use.")
 var webAddr = flag.String("webAddr", ":8080", "The bin address for the webserver.")
 
 const newDBQuery = `
@@ -54,7 +58,27 @@ type delegate struct{}
 
 func (d *delegate) NodeMeta(limit int) []byte { return nil }
 func (d *delegate) NotifyMsg(msg []byte) {
-	log.Printf("Message %s", msg)
+	i := bytes.IndexByte(msg, byte(':'))
+	if i == -1 {
+		log.Printf("Bad message: %s", msg)
+		return
+	}
+	typ := string(msg[:i])
+	msg = msg[i+1:]
+	log.Printf("Message (%s): %s", typ, msg)
+
+	switch typ {
+	case "AddTriplesRequest":
+		var req AddTriplesRequest
+		proto.Unmarshal(msg, &req)
+		for _, triple := range req.Triples {
+			log.Printf("Triple %#v", triple)
+			err := insertTriple(triple)
+			if err != nil {
+				log.Printf("err inserting: %s", err.Error())
+			}
+		}
+	}
 }
 func (d *delegate) GetBroadcasts(overhead, limit int) [][]byte { return nil }
 func (d *delegate) LocalState(join bool) []byte                { return nil }
@@ -77,6 +101,9 @@ func main() {
 	config.Delegate = del
 	log.Printf("Listening on %s:%d", config.BindAddr, config.BindPort)
 	config.BindPort = *bindPort
+	if len(*bindAddr) > 0 {
+		config.Name = *bindAddr
+	}
 	list, err := memberlist.Create(config)
 	if err != nil {
 		log.Fatal("Failed to create memberlist: " + err.Error())
@@ -92,10 +119,42 @@ func main() {
 	}
 
 	for _, member := range list.Members() {
-		log.Printf("Node: %s %s\n", member.Name, member.Addr)
+		log.Printf("Node: %s:%d %s\n", member.Name, member.Port, member.Addr)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/api/v1/query", func(w http.ResponseWriter, r *http.Request) {})
+	http.HandleFunc("/api/v1/insert", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "endpoint needs POST", 400)
+			return
+		}
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		subj := r.FormValue("subj")
+		pred := r.FormValue("pred")
+		obj := r.FormValue("obj")
+		triple := &Triple{
+			subj, pred, obj,
+		}
+		req := &AddTriplesRequest{
+			&Node{},
+			[]*Triple{triple},
+		}
+		members := list.Members()
+		node := members[rand.Intn(len(members))]
+		out, err := proto.Marshal(req)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		packaged := append([]byte("AddTriplesRequest:"), out...)
+		list.SendToTCP(node, packaged)
+	})
 	http.HandleFunc("/api/v1/status", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "running")
 	})
--- main.pb.go
+++ main.pb.go
@@ -11,6 +11,10 @@ It is generated from these files:
 It has these top-level messages:
 	TripleSet
 	Triple
+	Node
+	Query
+	Filter
+	AddTriplesRequest
 */
 package main
 
@@ -22,6 +26,29 @@ import math "math"
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+
+type Filter_Type int32
+
+const (
+	Filter_EXISTS    Filter_Type = 0
+	Filter_EQUAL     Filter_Type = 1
+	Filter_NOT_EQUAL Filter_Type = 2
+)
+
+var Filter_Type_name = map[int32]string{
+	0: "EXISTS",
+	1: "EQUAL",
+	2: "NOT_EQUAL",
+}
+var Filter_Type_value = map[string]int32{
+	"EXISTS":    0,
+	"EQUAL":     1,
+	"NOT_EQUAL": 2,
+}
+
+func (x Filter_Type) String() string {
+	return proto.EnumName(Filter_Type_name, int32(x))
+}
 
 type TripleSet struct {
 	Triples []*Triple `protobuf:"bytes,1,rep,name=triples" json:"triples,omitempty"`
@@ -39,11 +66,79 @@ func (m *TripleSet) GetTriples() []*Triple {
 }
 
 type Triple struct {
-	Subj string `protobuf:"bytes,1,opt,name=Subj" json:"Subj,omitempty"`
-	Pred string `protobuf:"bytes,2,opt,name=Pred" json:"Pred,omitempty"`
-	Obj  string `protobuf:"bytes,3,opt,name=Obj" json:"Obj,omitempty"`
+	Subj string `protobuf:"bytes,1,opt,name=subj" json:"subj,omitempty"`
+	Pred string `protobuf:"bytes,2,opt,name=pred" json:"pred,omitempty"`
+	Obj  string `protobuf:"bytes,3,opt,name=obj" json:"obj,omitempty"`
 }
 
 func (m *Triple) Reset()         { *m = Triple{} }
 func (m *Triple) String() string { return proto.CompactTextString(m) }
 func (*Triple) ProtoMessage()    {}
+
+type Node struct {
+	Name string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
+}
+
+func (m *Node) Reset()         { *m = Node{} }
+func (m *Node) String() string { return proto.CompactTextString(m) }
+func (*Node) ProtoMessage()    {}
+
+type Query struct {
+	Source *Node     `protobuf:"bytes,1,opt,name=source" json:"source,omitempty"`
+	Filter []*Filter `protobuf:"bytes,2,rep,name=filter" json:"filter,omitempty"`
+}
+
+func (m *Query) Reset()         { *m = Query{} }
+func (m *Query) String() string { return proto.CompactTextString(m) }
+func (*Query) ProtoMessage()    {}
+
+func (m *Query) GetSource() *Node {
+	if m != nil {
+		return m.Source
+	}
+	return nil
+}
+
+func (m *Query) GetFilter() []*Filter {
+	if m != nil {
+		return m.Filter
+	}
+	return nil
+}
+
+type Filter struct {
+	Pred string      `protobuf:"bytes,1,opt,name=pred" json:"pred,omitempty"`
+	Obj  string      `protobuf:"bytes,2,opt,name=obj" json:"obj,omitempty"`
+	Type Filter_Type `protobuf:"varint,3,opt,name=type,enum=Filter_Type" json:"type,omitempty"`
+}
+
+func (m *Filter) Reset()         { *m = Filter{} }
+func (m *Filter) String() string { return proto.CompactTextString(m) }
+func (*Filter) ProtoMessage()    {}
+
+type AddTriplesRequest struct {
+	Source  *Node     `protobuf:"bytes,1,opt,name=source" json:"source,omitempty"`
+	Triples []*Triple `protobuf:"bytes,2,rep,name=triples" json:"triples,omitempty"`
+}
+
+func (m *AddTriplesRequest) Reset()         { *m = AddTriplesRequest{} }
+func (m *AddTriplesRequest) String() string { return proto.CompactTextString(m) }
+func (*AddTriplesRequest) ProtoMessage()    {}
+
+func (m *AddTriplesRequest) GetSource() *Node {
+	if m != nil {
+		return m.Source
+	}
+	return nil
+}
+
+func (m *AddTriplesRequest) GetTriples() []*Triple {
+	if m != nil {
+		return m.Triples
+	}
+	return nil
+}
+
+func init() {
+	proto.RegisterEnum("Filter_Type", Filter_Type_name, Filter_Type_value)
+}
