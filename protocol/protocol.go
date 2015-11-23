d74152fe152e5d9930d6013a4aedc0396968ecd1
Tristan Rice <rice@fn.lc>
2015-11-23 09:56:52 -0800

    Added Mag, Union and Maxed commands to protocol.Keyspace

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -4,11 +4,53 @@ import "github.com/spaolacci/murmur3"
 
 //go:generate protoc --gogoslick_out=. protocol.proto
 
+// Includes checks if the provided uint64 is inside the keyspace.
 func (k *Keyspace) Includes(hash uint64) bool {
 	a := hash
 	s := k.Start
 	e := k.End
 	return s <= a && a < e || a < e && e < s || e < s && s <= a
+}
+
+// Mag returns the size of the keyspace.
+func (k *Keyspace) Mag() uint64 {
+	return k.End - k.Start
+}
+
+// Union returns the union of the keyspace. They must overlap otherwise nil is returned.
+func (k *Keyspace) Union(a *Keyspace) *Keyspace {
+	aSI := k.Includes(a.Start) || k.End == a.Start
+	aEI := k.Includes(a.End) || k.Start == a.End
+	kSI := a.Includes(k.Start) || a.End == k.Start
+	kEI := a.Includes(k.End) || a.Start == k.End
+
+	switch {
+	// Complete keyspace
+	case aSI && aEI && kSI && kEI:
+		return &Keyspace{Start: k.Start, End: k.Start - 1}
+
+	// k encompasses a
+	case aSI && aEI:
+		return &Keyspace{Start: k.Start, End: k.End}
+
+	// a encompasses k
+	case kSI && kEI:
+		return &Keyspace{Start: a.Start, End: a.End}
+
+	// a.Start is in k
+	case aSI:
+		return &Keyspace{Start: k.Start, End: a.End}
+
+	// a.End is in k
+	case aEI:
+		return &Keyspace{Start: a.Start, End: k.End}
+	}
+	return nil
+}
+
+// Maxed returns whether the keyspace encompasses the entire keyspace.
+func (k *Keyspace) Maxed() bool {
+	return k.End == k.Start-1
 }
 
 func (msg *Message) Hash() uint64 {
--- protocol/protocol_test.go
+++ protocol/protocol_test.go
@@ -1,8 +1,11 @@
 package protocol
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func TestIncludes(t *testing.T) {
+func TestKeyspaceIncludes(t *testing.T) {
 	testData := []struct {
 		ks   *Keyspace
 		hash uint64
@@ -60,6 +63,114 @@ func TestIncludes(t *testing.T) {
 	for i, td := range testData {
 		if out := td.ks.Includes(td.hash); out != td.want {
 			t.Errorf("%d. %#v.Includes(%#v) = %#v not %#v", i, td.ks, td.hash, out, td.want)
+		}
+	}
+}
+
+func TestKeyspaceUnion(t *testing.T) {
+	testData := []struct {
+		a, b, want *Keyspace
+	}{
+		{
+			&Keyspace{1, 10},
+			&Keyspace{20, 30},
+			nil,
+		},
+		{
+			&Keyspace{1, 10},
+			&Keyspace{10, 20},
+			&Keyspace{1, 20},
+		},
+		{
+			&Keyspace{10, 20},
+			&Keyspace{1, 10},
+			&Keyspace{1, 20},
+		},
+		{
+			&Keyspace{1, 20},
+			&Keyspace{5, 10},
+			&Keyspace{1, 20},
+		},
+		{
+			&Keyspace{5, 10},
+			&Keyspace{1, 20},
+			&Keyspace{1, 20},
+		},
+		{
+			&Keyspace{math.MaxUint64 - 5, math.MaxUint64 - 1},
+			&Keyspace{math.MaxUint64 - 1, 20},
+			&Keyspace{math.MaxUint64 - 5, 20},
+		},
+		{
+			&Keyspace{math.MaxUint64 - 1, 20},
+			&Keyspace{math.MaxUint64 - 5, math.MaxUint64 - 1},
+			&Keyspace{math.MaxUint64 - 5, 20},
+		},
+		{
+			&Keyspace{math.MaxUint64 - 5, 1},
+			&Keyspace{1, 20},
+			&Keyspace{math.MaxUint64 - 5, 20},
+		},
+		{
+			&Keyspace{1, 20},
+			&Keyspace{math.MaxUint64 - 5, 1},
+			&Keyspace{math.MaxUint64 - 5, 20},
+		},
+		{
+			&Keyspace{1, 20},
+			&Keyspace{20, 1},
+			&Keyspace{1, 0},
+		},
+	}
+	for i, td := range testData {
+		if out := td.a.Union(td.b); !out.Equal(td.want) {
+			t.Errorf("%d. %+v.Union(%+v) = %+v not %+v", i, td.a, td.b, out, td.want)
+		}
+	}
+}
+
+func TestKeyspaceMag(t *testing.T) {
+	testData := []struct {
+		a    *Keyspace
+		want uint64
+	}{
+		{
+			&Keyspace{1, 10},
+			9,
+		},
+		{
+			&Keyspace{math.MaxUint64 - 5, 1},
+			7,
+		},
+	}
+	for i, td := range testData {
+		if out := td.a.Mag(); out != td.want {
+			t.Errorf("%d. %+v.Mag() = %+v not %+v", i, td.a, out, td.want)
+		}
+	}
+}
+
+func TestMaxed(t *testing.T) {
+	testData := []struct {
+		a    *Keyspace
+		want bool
+	}{
+		{
+			&Keyspace{1, 10},
+			false,
+		},
+		{
+			&Keyspace{2, 1},
+			true,
+		},
+		{
+			(&Keyspace{1, 20}).Union(&Keyspace{20, 1}),
+			true,
+		},
+	}
+	for i, td := range testData {
+		if out := td.a.Maxed(); out != td.want {
+			t.Errorf("%d. %+v.Maxed() = %+v not %+v", i, td.a, out, td.want)
 		}
 	}
 }
